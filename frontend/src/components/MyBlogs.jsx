11f43b2748fad7b78541b2b33e14f1c019c28589
sai Vishveshwar Rao <vishveshwarkodipelly@gmail.com>
2024-06-21 17:40:46 +0530

    final commit

--- frontend/src/components/MyBlogs.jsx
+++ frontend/src/components/MyBlogs.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-// import { FaEdit } from "react-icons/fa";
 import { Button, Card } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 
 const MyBlogs = ({ blog }) => {
-  // const navigate = useNavigate();
-
-  // const handleClick = () => {
-  //   navigate(`/blogs/${blog._id}/edit`);
-  // };
   return (
     <Card
       className="my-3 p-3 rounded"
@@ -17,6 +11,7 @@ const MyBlogs = ({ blog }) => {
       <Link to={`/blogs/${blog._id}`}>
         <Card.Img src={blog.image} alt={blog.content} variant="top" />
       </Link>
+
       <Card.Body>
         <Link
           to={`/blogs/${blog._id}`}
@@ -26,13 +21,12 @@ const MyBlogs = ({ blog }) => {
             <strong>{blog.title}</strong>
           </Card.Title>
         </Link>
+
         <Card.Title as="div">
           <strong> - {blog.author}</strong>
         </Card.Title>
+
         <Card.Text as="div">{blog.content}</Card.Text>
-        {/* <Button variant="dark" className="mt-3" onClick={handleClick}>
-          <FaEdit /> Edit
-        </Button> */}
       </Card.Body>
     </Card>
   );
